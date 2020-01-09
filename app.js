15799c6b1702e6beabbc09874c4a0532918a4819
Patrick Moore <patrickwmoore88@gmail.com>
2020-01-09 10:44:41 -0600

    WIP

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "La Li Lu Le Lo",
+    secret: process.env.SERCET,
     resave: false,
     saveUninitialized: true,
     unset: "destroy"
