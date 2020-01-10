7ead3ede97174e1f4ecc8784a83c62cc168114d8
Patrick Moore <patrickwmoore88@gmail.com>
2020-01-10 12:44:44 -0600

    Will deploy now

---
+++ config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "apex",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    url: process.env.DB_URL,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres"
+  }
+};
