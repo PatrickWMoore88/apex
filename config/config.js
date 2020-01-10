2d975e977eb9adfa8f3098a2face289f5e8e749c
Patrick Moore <patrickwmoore88@gmail.com>
2020-01-10 12:22:38 -0600

    Will deploy now

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DB_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
