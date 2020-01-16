5a39111915f6f53fd36c803d732ee53519b9be7e
Patrick Moore <patrickwmoore88@gmail.com>
2020-01-16 11:12:25 -0600

    fixed loading issue with local site

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: process.env.SERCET,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     unset: "destroy"
